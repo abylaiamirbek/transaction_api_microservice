cec8eecaed5fc70c72da5432a643f1b294be283b
Abylai <amirbekabylai04@gmail.com>
2024-08-05 00:49:47 +0500

    init

--- src/main/java/kz/abylai/solva/TransactionApiApplication.java
+++ src/main/java/kz/abylai/solva/TransactionApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SolvaMcrApplication {
+public class TransactionApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SolvaMcrApplication.class, args);
+		SpringApplication.run(TransactionApiApplication.class, args);
 	}
 
 }
